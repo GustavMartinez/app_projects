5f48c1a232dc8a5e13d1f0dd106a2e6e623cfd34
GustavMartinez <gustavo.a.t.m.12345@hotmail.com>
2025-05-17 03:52:18 -0300

    Adicion de la logica de seleccion

--- 01_mokepon_platzi/js/script.js
+++ 01_mokepon_platzi/js/script.js
@@ -1,7 +1,25 @@
-function seleccionarMascotaJugador(){
-    alert("Seleccionaste la mascota")
+function iniciarJuego(){
+    let botonMascotaJugador = document.getElementById('boton-mascota') // Selecciona el elemento boton-mascota
+    botonMascotaJugador.addEventListener('click', seleccionarMascotaJugador)
 }
 
-let botonMascotaJugador = document.getElementById('boton-mascota') // Selecciona el elemento boton-mascota
 
-botonMascotaJugador.addEventListener('click', seleccionarMascotaJugador)
+function seleccionarMascotaJugador(){
+
+    let inputHipodoge = document.getElementById('hipodoge')
+    let inputCapipepo = document.getElementById('capipepo')
+    let inputRatigueya = document.getElementById('retigueya')
+
+    if (inputHipodoge.cheked){
+        alert('Seleccionaste a Hipodoge')
+    }else if(inputCapipepo.cheked){
+        alert('Seleccionaste a Capipepo')
+    }else if(inputRatigueya.cheked){
+        alert('Seleccionaste a Ratigueya')
+    }else{
+        alert('Selecciona una mascota')
+    }
+}
+
+
+window.addEventListener('load', iniciarJuego)
