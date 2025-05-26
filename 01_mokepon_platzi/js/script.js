8ece8cfa4ac294778c697dc470db1d5a69540b3e
Gustavo <gustavo.a.t.m.12345@hotmail.com>
2025-05-26 17:59:16 -0300

    nuevas funciones

--- 01_mokepon_platzi/js/script.js
+++ 01_mokepon_platzi/js/script.js
@@ -8,20 +8,32 @@ function seleccionarMascotaJugador(){
 
     let inputHipodoge = document.getElementById('hipodoge')
     let inputCapipepo = document.getElementById('capipepo')
-    let inputRatigueya = document.getElementById('retigueya')
-    //let spanMascotaJugador = document.getElementById('mascota-jugador')
+    let inputRatigueya = document.getElementById('ratigueya')
+    let spanMascotaJugador = document.getElementById('mascota-jugador')
 
     if (inputHipodoge.checked){
-        alert('Seleccionaste a Hipodoge')
-        //spanMascotaJugador.innerHTML = 'Hipodogue'
+        //alert('Seleccionaste a Hipodoge')
+        spanMascotaJugador.innerHTML = 'Hipodogue'
     }else if(inputCapipepo.checked){
-        alert('Seleccionaste a Capipepo')
+        //alert('Seleccionaste a Capipepo')
+        spanMascotaJugador.innerHTML = 'Capipepo'
     }else if(inputRatigueya.checked){
-        alert('Seleccionaste a Ratigueya')
+        //alert('Seleccionaste a Ratigueya')
+        spanMascotaJugador.innerHTML = 'Ratigueya'
     }else{
         alert('Selecciona una mascota')
     }
+
+    seleccionarMascotaEnemigo()
 }
 
+function seleccionarMascotaEnemigo(){
+    
+}
+
+
+function aleatorio(min, max){
+    return Math.floor(Math.random() * (max-min+1) + min)
+}
 
 window.addEventListener('load', iniciarJuego)
